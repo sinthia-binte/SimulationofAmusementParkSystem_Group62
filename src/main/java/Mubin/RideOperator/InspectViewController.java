81ec590951a97101f06850f2b6c3d030275aaaf0
amanullahalmubin <mubingptplus@gmail.com>
2026-07-26 14:55:46 +0600

    fxml and controller class

---
+++ src/main/java/Mubin/RideOperator/InspectViewController.java
@@ -0,0 +1,26 @@
+package Mubin.RideOperator;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+public class InspectViewController {
+    @javafx.fxml.FXML
+    private ComboBox submitResultComboBox;
+    @javafx.fxml.FXML
+    private Label resultComboBox;
+    @javafx.fxml.FXML
+    private ComboBox checkedItemComboBox;
+
+    @javafx.fxml.FXML
+    public void backToDashboardButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitInspectionButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButtonOA(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/Mubin/RideOperator/ManageRideCycleViewController.java
@@ -0,0 +1,31 @@
+package Mubin.RideOperator;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class ManageRideCycleViewController {
+
+    @Deprecated
+    public void inspectSeatsMRCS(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backToDashboardMRCS(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void releaseRestraintsMRCS(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void assistExitMRCS(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void confirmStopMRCS(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void confirmReadyMRCS(ActionEvent actionEvent) {
+    }
+}
