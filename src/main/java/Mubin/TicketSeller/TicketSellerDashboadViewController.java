b9c9fac2897ad040c1a84235bbcb36d0e28c798f
amanullahalmubin <mubingptplus@gmail.com>
2026-07-26 13:05:56 +0600

    FXML
    Dashboad

---
+++ src/main/java/Mubin/TicketSeller/TicketSellerDashboadViewController.java
@@ -0,0 +1,41 @@
+package Mubin.TicketSeller;
+
+import javafx.event.ActionEvent;
+
+public class TicketSellerDashboadViewController {
+    @javafx.fxml.FXML
+    public void sellTicketButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateTicketButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void processGroupBookingButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewTransactionButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void checkInventoryButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void processRefundButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void eodReportButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void applyPromoCodeButtonOA(ActionEvent actionEvent) {
+    }
+}
