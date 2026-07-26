00278ac31503ca9660ba0d8ee5c199dd43f5d178
amanullahalmubin <mubingptplus@gmail.com>
2026-07-26 12:34:16 +0600

    Create LoginViewController.java

---
+++ src/main/java/Mubin/LoginViewController.java
@@ -0,0 +1,19 @@
+package Mubin;
+
+public class LoginViewController
+{
+    @javafx.fxml.FXML
+    private TextField userIdTF;
+    @javafx.fxml.FXML
+    private Label errorMsgLabel;
+    @javafx.fxml.FXML
+    private TextField passwordTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void loginButtonOA(ActionEvent actionEvent) {
+    }
+}
