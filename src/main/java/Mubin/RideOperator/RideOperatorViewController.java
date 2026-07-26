f29fc85428eedeb88fc6b894f46445ff5f153f7a
amanullahalmubin <mubingptplus@gmail.com>
2026-07-26 13:25:51 +0600

    fxml and controller
    Ride Operator

---
+++ src/main/java/Mubin/RideOperator/RideOperatorViewController.java
@@ -0,0 +1,42 @@
+package Mubin.RideOperator;
+
+import javafx.event.ActionEvent;
+
+public class RideOperatorViewController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void manageRideCycleOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void enforceRestrictionsOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void inspectRideOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reportIncidentOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewScheduleButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void operateRideOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageQueueOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageBoardingOA(ActionEvent actionEvent) {
+    }
+}
